5234d7bc1de1311fdc8b475903201340cb0f9b28
thehuntedpie <thehuntedpie@gmail.com>
2018-09-25 22:39:42 +0300

    DRY, parameterize, try to fix fifo permissions, even though they dont matter

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -1,17 +1,20 @@
 #!/bin/bash -x
-# change ownership of home directory
-chown mindustry:mindustry -R ~mindustry
 
-# become non-elevated user
-#su mindustry
+MINDUSTRY_USER=mindustry
+MINDUSTRY_GROUP=$(su $MINDUSTRY_USER 'id -g')
+MINDUSTRY_HOME=$(su $MINDUSTRY_USER 'echo $HOME')
 
 # define server command
 SERVER_CMD='java -jar /usr/local/share/mindustry/server.jar host'
+TMUX_SESSION=Mindustry
 
-# run host in tmux
-su mindustry bash -c "tmux new-session -s Mindustry '${SERVER_CMD}'"
-#su mindustry bash -c "screen -m -S Mindustry '${SERVER_CMD}'"
+# change ownership of home directory
+chown ${MINDUSTRY_USER}:${MINDUSTRY_GROUP} -R ${MINDUSTRY_HOME}
 
-mkfifo /tmp/tmuxpipe
-tmux pipe-pane -o -t my_tmux_session 'cat >> /tmp/tmuxpipe'
+# become non-elevated user and run host in tmux
+su ${MINDUSTRY_USER} bash -c "tmux new-session -s ${TMUX_SESSION} '${SERVER_CMD}'"
+
+# make pipe file for docker logs (may solve issues showing raw tmux output)
+su ${MINDUSTRY_USER} bash -c "mkfifo /tmp/tmuxpipe"
+su ${MINDUSTRY_USER} bash -c "tmux pipe-pane -o -t ${TMUX_SESSION} 'cat >> /tmp/tmuxpipe'"
 cat /tmp/tmuxpipe
