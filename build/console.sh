71bb89936e41846557ec57e6ab66a5a4113a0776
thehuntedpie <thehuntedpie@gmail.com>
2018-09-25 23:29:23 +0300

    parameterize and functionalize

--- build/console.sh
+++ build/console.sh
@@ -1,4 +1,11 @@
 #!/bin/bash
 COMMAND=$1
-su mindustry bash -c "tmux send-keys -t Mindustry -l ${COMMAND}"
-su mindustry bash -c "tmux send-keys -t Mindustry Enter"
+TMUX_SESSION=Mindustry
+MINDUSTRY_USER=mindustry
+
+function console_run() {
+    su ${MINDUSTRY_USER} bash -c "tmux send-keys -t ${TMUX_SESSION} -l ${COMMAND}"
+    su ${MINDUSTRY_USER} bash -c "tmux send-keys -t ${TMUX_SESSION} Enter"
+}
+
+console_run $COMMAND
